860d7ca88a958b46a34530a1726c04918e2e280d
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-02-06 05:45:06 -0600

    quitting the game

---
+++ src/PPS.UI/MainMenu.vb
@@ -0,0 +1,30 @@
+﻿Friend Module MainMenu
+    Const QuitText = "Quit"
+    Const NoText = "No"
+    Const YesText = "Yes"
+    Const MainMenuTitle = "[olive]Main Menu:[/]"
+    Const ConfirmQuitTitle = "[red]Are you sure you want to quit?[/]"
+    Friend Sub Run()
+        Do While RunLoop()
+        Loop
+    End Sub
+
+    Private Function RunLoop() As Boolean
+        AnsiConsole.Clear()
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = MainMenuTitle}
+        prompt.AddChoices(QuitText)
+        Dim answer = AnsiConsole.Prompt(prompt)
+        Select Case answer
+            Case QuitText
+                Return Not ConfirmQuit()
+            Case Else
+                Throw New NotImplementedException
+        End Select
+        Return True
+    End Function
+    Private Function ConfirmQuit() As Boolean
+        Dim prompt As New SelectionPrompt(Of String) With {.Title = ConfirmQuitTitle}
+        prompt.AddChoices(NoText, YesText)
+        Return AnsiConsole.Prompt(prompt) = YesText
+    End Function
+End Module
--- src/PPS.UI/Runner.vb
+++ src/PPS.UI/Runner.vb
@@ -1,5 +1,6 @@
 Public Module Runner
     Public Sub Run()
         TitleScreen.Run()
+        MainMenu.Run()
     End Sub
 End Module
