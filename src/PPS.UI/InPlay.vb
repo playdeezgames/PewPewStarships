75991d0f05e54c6abdfd13440756409bc52fa54b
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-02-07 05:52:33 -0600

    show faction and ships

--- src/PPS.UI/InPlay.vb
+++ src/PPS.UI/InPlay.vb
@@ -5,13 +5,23 @@ Friend Module InPlay
         Do While Not scenario.IsCompleted
             Dim faction = scenario.CurrentFaction
             If faction.IsHuman Then
-                'human turn
+                RunHumanTurn(faction)
             Else
                 'computer turn
             End If
             scenario.NextFaction()
         Loop
+        AnsiConsole.Clear()
         AnsiConsole.MarkupLine("Scenario Complete!")
+        OkPrompt()
+    End Sub
+
+    Private Sub RunHumanTurn(faction As IFaction)
+        AnsiConsole.Clear()
+        AnsiConsole.MarkupLine($"{faction.Name}'s turn:")
+        For Each ship In faction.Ships
+            AnsiConsole.MarkupLine($"({ship.X},{ship.Y})")
+        Next
         OkPrompt()
     End Sub
 End Module
