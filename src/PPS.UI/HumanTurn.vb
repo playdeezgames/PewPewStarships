22161221c6cc2b44b44b201c488cbe0eec224a4a
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-02-08 05:12:02 -0600

    show stuff for ship

--- src/PPS.UI/HumanTurn.vb
+++ src/PPS.UI/HumanTurn.vb
@@ -4,7 +4,7 @@
         AnsiConsole.Clear()
         AnsiConsole.MarkupLine($"{faction.Name}'s turn:")
         For Each ship In faction.Ships
-            AnsiConsole.MarkupLine($"{ship.Identifier} {ship.Name}: ({ship.X},{ship.Y})")
+            AnsiConsole.MarkupLine($"ID: {ship.Identifier}, Name: {ship.Name}, X: {ship.X:f}, Y: {ship.Y:f}, Heading: {ship.Heading:f}, Speed: {ship.Speed:f}")
         Next
         OkPrompt()
     End Sub
